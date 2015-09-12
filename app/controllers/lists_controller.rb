5cab5e271dc0abfec0b0c4b928e83bd154b9ea52
Ted <t4dek01@gmail.com>
2015-09-12 17:07:46 +0300

    Implement new list creation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,14 @@ class ListsController < ApplicationController
     lists = List.all
     render json: lists
   end
+  
+  def create
+    list = List.create(list_params)
+    render json: list
+  end
+  
+  private
+    def list_params
+      params.require(:list).permit(:title, :completed)
+    end
 end
